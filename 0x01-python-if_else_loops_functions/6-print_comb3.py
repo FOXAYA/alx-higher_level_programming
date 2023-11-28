99da59d6dfbd5b5c76e7f361180f4e7175ca688c
FOXAYA <omniasaged111@gmail.com>
2023-11-28 14:04:15 -0800

    updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for digit1 in range(10):
-    for digit2 in range(digit1 + 1, 10):
-        print(f"{digit1:}{digit2:}", end=', ' if digit2 < 9 else '\n')
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
