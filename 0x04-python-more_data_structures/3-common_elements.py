75cfd2c58467b2ae2a223574afa1fe760df3ffe6
FOXAYA <omniasaged111@gmail.com>
2023-12-06 12:18:51 -0800

    updated

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python
 def common_elements(set_1, set_2):
-    common_set = set()
-    for element in set_1:
-        if element in set_2:
-            common_set.add(element)
-            return common_set
+    return set_1 & set_2
