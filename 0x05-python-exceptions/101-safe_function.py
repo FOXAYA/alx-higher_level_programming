3f08fb61660fd4b826e50f58fd8b128db869aae4
FOXAYA <omniasaged111@gmail.com>
2023-12-18 12:42:57 -0800

    updated

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
