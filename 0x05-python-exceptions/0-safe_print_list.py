6aa107dbb26926de8aa11239de0e5c357afa8663
FOXAYA <omniasaged111@gmail.com>
2023-12-18 12:11:04 -0800

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list(my_list =[], x=0):
+def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
             count = count + 1
-    except:
+    Exception:
         print()
         return count
     else:
