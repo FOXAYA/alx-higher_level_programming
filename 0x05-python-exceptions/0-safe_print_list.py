d0768e80128c081854abf951ea6633bdd5789791
FOXAYA <omniasaged111@gmail.com>
2023-12-18 12:18:39 -0800

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
             count = count + 1
-    Exception:
+    except IndexError:
         print()
         return count
     else:
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    Exception:
+    except (ValueError, TypeError):
         return False
     else:
         return True
