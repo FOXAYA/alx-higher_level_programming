5643770db6f8f1a39db80bad1ada685e0a181602
FOXAYA <omniasaged111@gmail.com>
2024-01-08 11:39:56 -0800

    updated

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    3-is_kind_of_class.py
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Compare is obj is an instance of a_class"""
+    return isinstance(obj, a_class)
