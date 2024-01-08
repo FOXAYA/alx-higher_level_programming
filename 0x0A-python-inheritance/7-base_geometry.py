ead37db3fac064dbc51701fc44680350093a0395
FOXAYA <omniasaged111@gmail.com>
2024-01-08 12:16:42 -0800

    updated

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    7-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def integer_validator(self, name, value):
+        """Method for validate if a num is integer"""
+        if type(value) != int:
+            raise TypeError(name +  "must be an integer")
+        if value <= 0:
+            raise ValueError( name + "must be greater than 0")
