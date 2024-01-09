29842595697bb7f4108f181bb5168f3e143c6777
FOXAYA <omniasaged111@gmail.com>
2024-01-09 04:38:24 -0800

    updated

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-class Square(Rectangle):
-    """Represent a square"""
 
+
+class Square(Rectangle):
+    """Square class"""
     def __init__(self, size):
-        """Initialize a nهew square
-        """
+        """Method for initializing a square"""
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Method that returns area of a square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Method that returns a string"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
