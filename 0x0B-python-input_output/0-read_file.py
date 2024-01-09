037e467ab8042b9836acceff5a599a7831a44ff5
FOXAYA <omniasaged111@gmail.com>
2024-01-09 12:23:17 -0800

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
+!/usr/bin/python3
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
