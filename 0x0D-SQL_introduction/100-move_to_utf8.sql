43f42ddec991301b57bfa6ecb827ca80add3885b
FOXAYA <omniasaged111@gmail.com>
2024-02-14 14:01:47 -0800

    updated

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Change character SET and COLLATE
+-- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+USE hbtn_0c_0
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE name name VARCHAR (256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
