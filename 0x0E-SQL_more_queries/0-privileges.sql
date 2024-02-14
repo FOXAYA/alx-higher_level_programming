2a576da9e8bc9f38e2c43758db23fd6e00ce9653
FOXAYA <omniasaged111@gmail.com>
2024-02-14 14:31:25 -0800

    updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- Print all privileges of USER
--- Execute: cat 0-privileges.sql | mysql -hlocalhost -uroot -p
-SHOW GRANTS FOR `user_0d_1`@`localhost`;
-SHOW GRANTS FOR `user_0d_2`@`localhost`;
-
+-- My privileges
+-- show privs for user1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- show privs for user2
+SHOW GRANTS FOR user_0d_2@localhost;
