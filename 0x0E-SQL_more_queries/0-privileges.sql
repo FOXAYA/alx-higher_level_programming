9e0968c0e7cb620c14d0bd0eb1d5753007393d57
FOXAYA <omniasaged111@gmail.com>
2024-02-14 14:16:14 -0800

    updated

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Print all privileges of USER
+-- Execute: cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
+
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Crate user
+-- Execute: cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
