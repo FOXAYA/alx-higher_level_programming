5dc91c644115da63e1986678c6400843d9d1a0f2
FOXAYA <omniasaged111@gmail.com>
2024-02-14 14:34:53 -0800

    updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- My privileges
--- show privs for user1
-SHOW GRANTS FOR user_0d_1@localhost;
--- show privs for user2
-SHOW GRANTS FOR user_0d_2@localhost;
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
