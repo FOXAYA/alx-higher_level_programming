4519cb4db298d05c4448ac156c22b0937887a32e
FOXAYA <omniasaged111@gmail.com>
2024-03-11 13:32:30 -0700

    updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+// Prints a message depending of the number of arguments passed
+
 if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
