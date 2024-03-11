321141f4b8c0b45be866f93f69e2aa6019e23f7d
FOXAYA <omniasaged111@gmail.com>
2024-03-11 13:05:07 -0700

    updated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,3 @@
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
-
-//other solution
-//console.log('C is fun \nPython is cool\nJavaScript is amazing');
