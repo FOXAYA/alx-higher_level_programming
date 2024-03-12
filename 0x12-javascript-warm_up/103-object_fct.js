c388e45e8efff39e1f39edf869737087445e321d
FOXAYA <omniasaged111@gmail.com>
2024-03-11 18:06:39 -0700

    updated

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,11 +4,7 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-
-function incr() {
-  this.value++;
-}
-
+myObject.incr = function () { myObject.value += 1; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
