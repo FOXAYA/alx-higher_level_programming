a0fac90eda57f9f4051077e59e0f9a4538f62c70
FOXAYA <omniasaged111@gmail.com>
2024-03-12 17:51:03 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-rectangle.js') {
-  charPrint(c) {
-   if (
-    
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
 };
