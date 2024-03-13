cb562215233208287198ab3bd5e201e72a409dd1
FOXAYA <omniasaged111@gmail.com>
2024-03-12 18:04:41 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+/*
+Your script must import dict from the file 101-data.js
+In the new dictionary:
+A key is a number of occurrences
+A value is the list of user ids
+Print the new dictionary at the end
+*/
+const occurence = require('./101-data').dict;
+const newDict = {};
+for (const key in occurence) {
+  if (newDict[occurence[key]] === undefined) {
+    newDict[occurence[key]] = [];
   }
+  newDict[occurence[key]].push(key);
 }
 console.log(newDict);
